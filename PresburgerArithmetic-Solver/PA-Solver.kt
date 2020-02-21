2254472729f03a326bed2a5ca806f097e3c09c54
mekty2012 <41375658+mekty2012@users.noreply.github.com>
2020-02-21 09:20:30 +0900

    Update PA

--- PresburgerArithmetic-Solver/PA-Solver.kt
+++ PresburgerArithmetic-Solver/PA-Solver.kt
@@ -13,6 +13,18 @@ data class Or(val lf:List<PresburgerFormula>) : PresburgerFormula()
 data class Exists(val q:String, val body:PresburgerFormula) : PresburgerFormula()
 data class Forall(val q:String, val body:PresburgerFormula) : PresburgerFormula()
 
+fun simplify(pa:PresburgerArithmetic) : Map<String?, Int> =
+  when (pa) {
+    is Var -> mapOf(pa.s to 1)
+    is Integer -> mapOf(null to pa.n)
+    is Add -> {
+      val first = simplify(pa.first)
+      val second = simplify(pa.second)
+      val newKeySet = first.keys + second.keys
+      return newKeySet.map{Pair(it, pa.first.getOrDefault(it, 0) + pa.second.getOrDefault(it, 0))}
+    }
+  }
+
 fun isClosed(f:PresburgerArithmetic, env:Set<String>) : Boolean =
   when (f) {
     is Var -> env.contains(f.s)
@@ -43,7 +55,7 @@ fun isQuantifierFree(f:PresburgerFormula) : Boolean =
   }
 
 fun compute(f:PresburgerArithmetic) : Int {
-    return when (f) {
+  return when (f) {
     is Var -> throw IllegalArgumentException("The formula is not closed.")
     is Integer -> f.n
     is Add -> compute(f.first) + compute(f.second)
@@ -51,7 +63,7 @@ fun compute(f:PresburgerArithmetic) : Int {
 }
 
 fun freeDecide(f:PresburgerFormula) : Boolean {
-    return when (f) {
+  return when (f) {
     is Eq -> compute(f.first) == compute(f.second)
     is Div -> (compute(f.multiple) % f.n) == 0
     is Neg -> !freeDecide(f.f)
@@ -59,4 +71,49 @@ fun freeDecide(f:PresburgerFormula) : Boolean {
     is Or -> f.lf.any{freeDecide(it)}
     else -> {throw IllegalArgumentException("The formula is not quantifier free.")}
   }
+}
+
+/*
+ * This function pushes all the negation to bottom case for quantifier free formula.
+ * More specifically, it uses De Morgan's law to push all negation, so that
+ * all negation appearing in formula's parameter f is either Eq or Div.
+ */
+fun pushNegation(f:PresburgerFormula, b:Boolean) : PresburgerFormula {
+  return when (f) {
+    is Neg -> pushNegation(f.f, !b)
+    is And -> if (b) And(f.lf.map{pushNegation(it, true)}) else Or(f.lf.map{pushNegation(it, false)})
+    is Or -> if (b) Or(f.lf.map{pushNegation(it, true)}) else And(f.lf.map{pushNegation(it, false)})
+    is Exists -> throw IllegalArgumentException("The formula is not quantifier free.")
+    is Forall -> throw IllegalArgumentException("The formula is not quantifier free.")
+    else -> {if (b) f else Neg(f)}
+  }
+}
+
+/*
+ * This function returns equivalent DNF form for quantifier free formula, having all its negation to bottom case.
+ */
+fun convertDNF(f:PresburgerFormula) : List<List<PresburgerFormula>> {
+  return when (f) {
+    is Neg -> listOf() // If we meet negation, it is already literal. So we just simply skip.
+    is Or -> f.lf.map{convertDNF(it)}.flatten()
+    is And -> f.lf.fold(listOf(listOf())){dnf : List<List<PresburgerFormula>>, curr:PresburgerFormula ->
+      val currDNF = convertDNF(curr)
+      val result = mutableListOf<List<PresburgerFormula>>()
+      for (clauseFst in dnf) {
+        for (clauseSnd in currDNF) {
+          result.add(clauseFst + clauseSnd)
+        }
+      }
+      return result
+    } // This case is where exponentially increasing happens.
+    else -> {throw IllegalArgumentException("The formula is not quantifier free.")}
+  }
+}
+
+/*
+ * This function returns formula that is equivalent to exists x, f, where f is quantifier free formula.
+ * The input for f is expected to be DNF form.
+ */
+fun equivExist(dnf:List<List<PresburgerFormula>>, x:String) : PresburgerFormula {
+  TODO()
 }
--- PresburgerArithmetic-Solver/README.md
+++ PresburgerArithmetic-Solver/README.md
@@ -28,4 +28,50 @@ proof of universal quantifier is not simple.
 If possible, actually, the evidence of universal quantifier is usually given as function, as evidence of forall x, P x is function mapping x to evidence of P x.
 However, I will treat this problem later.
 
-For specific algorithm, you can see [here](https://github.com/mekty2012/sumunyeon/blob/master/19KPUmathseminar.pdf).
+For specific algorithm, you can see [here](https://github.com/mekty2012/sumunyeon/blob/master/SeminarProof.pdf).
+
+## Evidence Finding
+
+First, we define 'evidence' for base case.
+
+- a = b : the value a = b is evidence.
+- a | b : the value c that b = a * c is evidence.
+- C1 /\ C2 : pair of evidence for C1 and C2 is evidence.
+- C1 \\/ C2 : If C1 is true with evidence E1, (1, E1) is evidence. Otherwise, (2, E2) is evidence.
+- ~C : We expect our formula to have all its negation in base case. So, we can think as Not case as
+  ~(a=b), ~(a|b). 
+  + ~ (a=b) : the value a - b is evidence.
+  + ~ (a|b) : the value b % a is evidence.
+- Forall x. C : the function mapping x to evidence of C is evidence.
+- Exists x. C : the pair of value of x and evidence of C x is evidence.
+
+Problems : 
+The algorithm presented above cannot solve forall statement, therefore transform Forall x. C into ~(Exists x. ~C).
+Maybe it could be possible to create counter example class.
+
+Counterexample
+
+- ~ (a=b) : the value a - b is counterexample.
+- ~ (a|b) : the value b % a is counterexample.
+- ~ (C1 /\ C2) : If C1 is false with counterexample E1, (1, E1) is counterexample. Otherwise, (2, E2) is counterexample.
+- ~ (C1 \\/ C2) : pair of counterexample for C1 and C2 is counterexample.
+- ~ (~ C) : evidence of C is counterexample.
+- ~ (Forall x.C) : pair of value of x and counterexample of C x is counterexample.
+
+Merging evidence and counter example
+
+Finding evidence for formula F
+- (a = b) : the value a = b
+- (a | b) : the value c s.t. b = a * c
+- (C1 /\ C2) : the pair of evidence C1 and C2, (E1, E2)
+- (C1 \\/ C2) : pair of index and evidence (i, Ei)
+- (~ C) : counterexample of C
+- Exists x. C : pair of x and evidence of C x
+
+Finding counterexample for formula F
+- (a = b) : the value b - a
+- (a | b) : the value b % a
+- (C1 /\ C2) : pair of index and counterexample
+- (C1 \\/ C2) : pair of counterexamples
+- (~ C) : evidence of C
+- (Exists x. C) : function from x to counterexample of C x.
